d24b798592c9d9fb01082e998275973a0c70e4a1
jmf3658 <jfullmer@austin.utexas.edu>
2018-08-10 14:42:14 -0500

    Implement Image Link A & B migration

--- src/Plugin/migrate/destination/FlexPageFieldsDestination.php
+++ src/Plugin/migrate/destination/FlexPageFieldsDestination.php
@@ -37,6 +37,8 @@ class FlexPageFieldsDestination extends Entity implements MigrateDestinationInte
       $node->set('field_flex_page_wysiwyg_b', $row->getSourceProperty('wysiwyg_b'));
       $node->set('field_flex_page_fca_a', $row->getSourceProperty('fca_a'));
       $node->set('field_flex_page_fca_b', $row->getSourceProperty('fca_b'));
+      $node->set('field_flex_page_il_a', $row->getSourceProperty('image_link_a'));
+      $node->set('field_flex_page_il_b', $row->getSourceProperty('image_link_b'));
       $node->set('field_flex_page_pca', $row->getSourceProperty('photo_content_area'));
       $node->set('field_flex_page_pu', $row->getSourceProperty('promo_units'));
       $node->set('field_flex_page_ql', $row->getSourceProperty('quick_links'));

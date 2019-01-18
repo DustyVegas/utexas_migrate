8ba9ddfac3d4df470f8a5e4de1a75924707ae052
Mark Fullmer <mfullmer@gmail.com>
2019-01-18 09:54:53 -0600

    Convert resources migration to custom compound field schema

--- src/CustomWidgets/Resource.php
+++ src/CustomWidgets/Resource.php
@@ -3,11 +3,10 @@
 namespace Drupal\utexas_migrate\CustomWidgets;
 
 use Drupal\Core\Database\Database;
-use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\utexas_migrate\MigrateHelper;
 
 /**
- * Convert D7 custom compound field to D8 paragraph.
+ * Convert D7 custom compound field to D8 field.
  */
 class Resource {
 
@@ -18,12 +17,12 @@ class Resource {
    *   The node ID from the source data.
    *
    * @return array
-   *   Returns an array of Paragraph ID(s) of the widget.
+   *   Returns an array of custom compound field data.
    */
   public static function convert($source_nid) {
     $source_data = self::getSourceData($source_nid);
-    $paragraph_data = self::save($source_data);
-    return $paragraph_data;
+    $field_data = self::massageFieldData($source_data);
+    return $field_data;
   }
 
   /**
@@ -33,7 +32,7 @@ class Resource {
    *   The node ID from the source data.
    *
    * @return array
-   *   Returns an array of Paragraph ID(s) of the widget
+   *   Returns a prepared array of D8 custom compound field data.
    */
   public static function getSourceData($source_nid) {
     // Get all instances from the legacy DB.
@@ -56,69 +55,46 @@ class Resource {
   }
 
   /**
-   * Save data as paragraph(s) & return the paragraph ID(s)
+   * Rearrange schema as needed from D7 to D8.
    *
    * @param array $source
    *   A simple key-value array of subfield & value.
    *
    * @return array
-   *   A simple key-value array returned the metadata about the paragraph.
+   *   A simple key-value array of D8 field data.
    */
-  protected static function save(array $source) {
-    $paragraphs = [];
-    // First save the resource instances as paragraphs & retrieve their IDs.
+  protected static function massageFieldData(array $source) {
+    $destination = [];
+    if (isset($source[0]['title'])) {
+      $destination['headline'] = $source[0]['title'];
+    }
+    $items = [];
     foreach ($source as $delta => $instance) {
-      $field_values = [
-        'type' => 'utexas_resource',
-        'field_utexas_resource_headline' => [
-          'value' => $instance['headline'],
-        ],
-      ];
+      if (isset($instance['headline'])) {
+        $items[$delta]['item']['headline'] = $instance['headline'];
+      }
       if ($instance['image_fid'] != 0) {
-        $destination_fid = MigrateHelper::getMediaIdFromFid($instance['image_fid']);
-        $field_values['field_utexas_resource_image'] = [
-          'target_id' => $destination_fid,
-          'alt' => '@to be replaced with media reference',
-        ];
+        $destination_mid = MigrateHelper::getMediaIdFromFid($instance['image_fid']);
+        $items[$delta]['item']['image'] = $destination_mid;
       }
       $links = unserialize($instance['links']);
       if (!empty($links)) {
-        foreach ($links as $delta => $link) {
+        $prepared_links = [];
+        foreach ($links as $i => $link) {
           $prepared_links[] = [
-            'uri' => MigrateHelper::prepareLink($link['link_url']),
+            'url' => MigrateHelper::prepareLink($link['link_url']),
             'title' => $link['link_title'],
-            'delta' => $delta,
           ];
         }
-        $field_values['field_utexas_resource_links'] = $prepared_links;
+        if (!empty($prepared_links)) {
+          $items[$delta]['item']['links'] = $prepared_links;
+        }
       }
-      $paragraph_instance = Paragraph::create($field_values);
-      $paragraph_instance->save();
-      $paragraphs[] = [
-        'target_id' => $paragraph_instance->id(),
-        'target_revision_id' => $paragraph_instance->id(),
-        'delta' => $delta,
-      ];
     }
-    if (!empty($paragraphs)) {
-      // Next, save a single Resource Container, with the above
-      // as its paragraph references.
-      // Note that the zeroth delta can be used for the title, since
-      // there is always only one title for Resources.
-      $paragraph_container = Paragraph::create([
-        'type' => 'utexas_resource_container',
-        'field_utexas_rc_title' => $source[0]['title'],
-        'field_utexas_rc_items' => $paragraphs,
-      ]);
-      $paragraph_container->save();
-      // Finally, return the *single* Resource Container to the node.
-      return [
-        'target_id' => $paragraph_container->id(),
-        'target_revision_id' => $paragraph_container->id(),
-        'delta' => 0,
-      ];
+    if (!empty($items)) {
+      $destination['resource_items'] = serialize($items);
     }
-
+    return $destination;
   }
 
 }

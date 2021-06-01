daf8044cf59df83accf531ed389879135de7c416
Polo Reynaga <lreynaga@austin.utexas.edu>
2021-06-01 12:51:15 -0500

    Update module in process validations

--- src/Plugin/migrate/process/PathAliasProcess.php
+++ src/Plugin/migrate/process/PathAliasProcess.php
@@ -20,13 +20,13 @@ class PathAliasProcess extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    if (\Drupal::moduleHandler()->moduleExists('utprof_content_type_profile') === FALSE && $row->get('type') === 'team_member') {
+    if (\Drupal::moduleHandler()->moduleExists('utprof_migrate') === FALSE && $row->get('type') === 'team_member') {
       throw new MigrateSkipRowException('Skipping team_member/profile nodes given utprof content type does not exist.');
     }
-    if (\Drupal::moduleHandler()->moduleExists('utnews_content_type_news') === FALSE && $row->get('type') === 'news') {
+    if (\Drupal::moduleHandler()->moduleExists('utnews_migrate') === FALSE && $row->get('type') === 'news') {
       throw new MigrateSkipRowException('Skipping news nodes given utnews content type does not exist.');
     }
-    if (\Drupal::moduleHandler()->moduleExists('utevent_content_type_event') === FALSE && $row->get('type') === 'event') {
+    if (\Drupal::moduleHandler()->moduleExists('utevent_migrate') === FALSE && $row->get('type') === 'event') {
       throw new MigrateSkipRowException('Skipping event nodes given utevent content type does not exist.');
     }
     return $value;

417b4f94e57374f118c99cb8f837a32bfd08bc2b
Mark Fullmer <jfullmer@austin.utexas.edu>
2021-07-02 08:26:11 -0500

    Migrate authmap values, when present (Resolves #106) (#198)

--- src/Plugin/migrate/destination/UserDestination.php
+++ src/Plugin/migrate/destination/UserDestination.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\utexas_migrate\Plugin\migrate\destination;
 
+use Drupal\Core\Database\Database;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\migrate\Row;
 use Drupal\user\Plugin\migrate\destination\EntityUser;
 
@@ -31,6 +33,46 @@ class UserDestination extends EntityUser {
       return [0];
     }
     return parent::import($row, $old_destination_id_values);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function save(ContentEntityInterface $entity, array $old_destination_id_values = []) {
+    $id = parent::save($entity, $old_destination_id_values);
+    self::mapAuthmapValue($id[0], $entity->getAccountName());
+    return $id;
+  }
+
+  /**
+   * Helper function to map externalauth values, if present.
+   *
+   * @param int $uid
+   *   The destination uid.
+   * @param string $username
+   *   The username.
+   */
+  protected static function mapAuthmapValue($uid, $username) {
+    $source_db = Database::getConnection('default', 'utexas_migrate');
+    if ($source_db->schema()->tableExists('authmap')) {
+      $query = $source_db->select('authmap', 'v')
+        ->fields('v', ['authname'])
+        ->condition('authname', $username, '=')
+        ->execute()
+        ->fetch();
+      if (!empty($query->authname)) {
+        $destination_db = Database::getConnection('default', 'default');
+        if ($destination_db->schema()->tableExists('authmap')) {
+          $destination_db->merge('authmap')
+            ->key(['uid' => $uid, 'provider' => 'simplesamlphp_auth'])
+            ->fields([
+              'authname' => $username,
+              'data' => serialize(NULL),
+            ])
+            ->execute();
+        }
+      }
+    }
   }
 
 }

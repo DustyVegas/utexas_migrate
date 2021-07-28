aba9cee48be9b6f724be0d7be8a84daa2da47d35
Mark Fullmer <jfullmer@austin.utexas.edu>
2021-07-28 12:39:48 -0500

    Migrate News listing page title (Resolves #211) (#213)

--- modules/utnews_migrate/src/Event/MigrateEventSubscriber.php
+++ modules/utnews_migrate/src/Event/MigrateEventSubscriber.php
@@ -5,6 +5,7 @@ namespace Drupal\utnews_migrate\Event;
 use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate\Event\MigrateImportEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\utexas_migrate\MigrateHelper;
 
 use Drupal\utnews\Permissions;
 
@@ -28,6 +29,11 @@ class MigrateEventSubscriber implements EventSubscriberInterface {
     if ($event->getMigration()->getPluginId() === 'utnews_nodes') {
       Permissions::assignPermissions('editor', 'utexas_content_editor');
       Permissions::assignPermissions('manager', 'utexas_site_manager');
+      $listing_page_title = MigrateHelper::getVariable('utexas_news_all_articles_title') ?? 'Latest News';
+      $config = \Drupal::service('config.factory')->getEditable('utnews_view_listing_page.config');
+      $config->set('page_title', $listing_page_title);
+      $config->save();
     }
   }
+
 }

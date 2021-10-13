0378530694f5aa71783d6e273fedf9849f18407d
jmf3658 <jfullmer@austin.utexas.edu>
2021-10-13 14:37:03 -0500

    Use provisioned database as starting point

--- scripts/saas-migrate.sh
+++ scripts/saas-migrate.sh
@@ -30,8 +30,8 @@ git clone git@github.austin.utexas.edu:eis1-wcs/pantheon_docksal_starter.git .do
 rm -rf .docksal/.git
 fin init
 fin config set HOSTING_SITE="$DESTINATION_SITE"
-# fin drush si utexas -y
-fin drush si utexas utexas_installation_options.default_content=NULL -y
+fin pull db -y
+# fin drush si utexas utexas_installation_options.default_content=NULL -y
 chmod 755 web/sites/default
 
 echo "Rsyncing files from $SOURCE_SITE..."
@@ -39,25 +39,25 @@ SOURCE_ID=$(terminus site:info "$SOURCE_SITE" --field=ID)
 rsync -rvlz --copy-unsafe-links --exclude=css --exclude=php --exclude=js --exclude=styles --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' live.$SOURCE_ID@appserver.live.$SOURCE_ID.drush.in:files/ web/sites/default/files
 fin drush cr
 
-echo "*************************"
-echo "Enabling SaaS elements..."
-echo "*************************"
-fin drush en utexas_role_site_manager
+# echo "*************************"
+# echo "Enabling SaaS elements..."
+# echo "*************************"
+# fin drush en utexas_role_site_manager
 
-fin drush -y en utprof utprof_block_type_profile_listing utprof_content_type_profile utprof_view_profiles utprof_vocabulary_groups utprof_vocabulary_tags
-fin drush utprof:grant --set=manager --role=utexas_site_manager
-fin drush utprof:grant --set=editor --role=utexas_content_editor
+# fin drush -y en utprof utprof_block_type_profile_listing utprof_content_type_profile utprof_view_profiles utprof_vocabulary_groups utprof_vocabulary_tags
+# fin drush utprof:grant --set=manager --role=utexas_site_manager
+# fin drush utprof:grant --set=editor --role=utexas_content_editor
 
-fin drush -y en utnews utnews_block_type_news_listing utnews_content_type_news utnews_view_listing_page utnews_vocabulary_authors utnews_vocabulary_categories utnews_vocabulary_tags
-fin drush utnews:grant --set=manager --role=utexas_site_manager
-fin drush utnews:grant --set=editor --role=utexas_content_editor
+# fin drush -y en utnews utnews_block_type_news_listing utnews_content_type_news utnews_view_listing_page utnews_vocabulary_authors utnews_vocabulary_categories utnews_vocabulary_tags
+# fin drush utnews:grant --set=manager --role=utexas_site_manager
+# fin drush utnews:grant --set=editor --role=utexas_content_editor
 
-fin drush -y en utevent utevent_block_type_event_listing utevent_content_type_event utevent_view_listing_page utevent_vocabulary_location utevent_vocabulary_tags
-fin drush utevent:grant --set=manager --role=utexas_site_manager
-fin drush utevent:grant --set=editor --role=utexas_content_editor
+# fin drush -y en utevent utevent_block_type_event_listing utevent_content_type_event utevent_view_listing_page utevent_vocabulary_location utevent_vocabulary_tags
+# fin drush utevent:grant --set=manager --role=utexas_site_manager
+# fin drush utevent:grant --set=editor --role=utexas_content_editor
 
-fin drush en utexas_saml_auth_helper -y
-fin drush config-set simplesamlphp_auth.settings activate 1 -y
+# fin drush en utexas_saml_auth_helper -y
+# fin drush config-set simplesamlphp_auth.settings activate 1 -y
 
 echo "Running migration..."
 composer require utexas/utexas_migrate:dev-develop

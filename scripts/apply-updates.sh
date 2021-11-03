4da5c2e3b0a0f0792f5a2800e63036369f90bf9e
jmf3658 <jfullmer@austin.utexas.edu>
2021-11-03 12:55:19 -0500

    Add upstream-updater script with site looping

---
+++ scripts/apply-updates.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+## Helper for use with Jenkins
+if [ -z "$terminus" ];
+then
+  terminus="terminus"
+fi
+
+run_updates_on_site() {
+  echo "Running updates on $1 ..."
+  # Update 'preview'
+  $terminus env:wake $1.preview
+  $terminus env:clear-cache $1.preview
+  $terminus upstream:updates:apply --updatedb --accept-upstream -- $1.preview
+  # $terminus drush $1.dev updb -y
+  # $terminus drush $1.dev cr
+
+  # Update 'dev'
+  # $terminus env:wake $1.dev
+  # $terminus env:clear-cache $1.dev
+  # $terminus upstream:updates:apply --updatedb --accept-upstream -- $1.dev
+  # $terminus drush $1.dev updb -y
+  # $terminus drush $1.dev cr
+}
+
+if [ -z "$1" ];
+then
+  echo 'You must supply a filename or site name as the first parameter (e.g. "quicksites-to-managed-sites.sh" or "utexas-whole-health")'
+  exit
+fi
+if [ -f "$1" ]; 
+then 
+  source $1
+  if [ -z "$SITES" ];
+  then
+    echo 'Your source file is missing required variables'
+    exit
+  fi
+  for SITE in "${SITES[@]}"
+  do
+    run_updates_on_site $SITE
+  done
+else
+  SITE="$1"
+  echo "Checking validity of Pantheon site..."
+  ID=$(terminus site:info "$SITE" --field=ID)
+  if [ ! $ID ];
+  then
+    echo "ERROR: No Pantheon site found matching $SITE"
+    echo "Exiting..."
+    return
+  else
+    run_updates_on_site $SITE
+  fi
+fi
+
+
+
+
+

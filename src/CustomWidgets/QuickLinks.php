c39e191dafeb39b35a6517b3574c2986e24001d3
jmf3658 <jfullmer@austin.utexas.edu>
2021-11-22 11:41:23 -0600

    Do not add border styling to Quick Links

--- src/CustomWidgets/QuickLinks.php
+++ src/CustomWidgets/QuickLinks.php
@@ -114,13 +114,6 @@ class QuickLinks {
         $instances['field'][$delta]['links'] = serialize($prepared_links);
       }
     }
-    // Quick Links should always display with border w/o background.
-    $instances['additional'] = [
-      'layout_builder_styles_style' => [
-        'utexas_border_without_background' => 'utexas_border_without_background',
-
-      ],
-    ];
     return $instances;
   }
 

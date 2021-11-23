cb3e7a4dc83c5e1220b5c996b4f7f896a5941bc5
Paul Grotevant <pfg@utexas.edu>
2021-11-23 16:36:30 -0600

    Add combined group script.

---
+++ scripts/saas/group-combined.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+./migrate-to-preview.sh utqs-baitlc utexas-baitlc https://baitlc.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-budget utexas-budget https://budget.utexas.edu
+./migrate-to-preview.sh utqs-business-contracts utexas-business-contracts,https://businesscontracts.utexas.edu
+./migrate-to-preview.sh utqs-chasp utexas-chasp https://chasp.lbj.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-child-family-research utexas-child-family-research https://childandfamilyresearch.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-commcouncil utexas-commcouncil https://commcouncil.utexas.org >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-compliance utexas-compliance https://compliance.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-covid-resource-guide utexas-whole-health https://covid.wholecommunities.utexas.edu >> migration-report.txt 2>&1
+./migrate-to-preview.sh utqs-csrd utexas-csrd https://csrd.lbj.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-datacenters utexas-datacenters https://datacenters.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-dobie-paisano utexas-dobie-paisano https://dobiepaisano.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-faculty-council utexas-faculty-council https://facultycouncil.utexas.edu
+./migrate-to-preview.sh utqs-health-ipe utexas-health-ipe https://healthipe.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-impact-factory utexas-impact-factory https://theimpactfactory.org >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-internal-audits utexas-internal-audits https://audit.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-it-governance utexas-itlc https://itlc.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-kbh utexas-kbh https://kbhenergycenter.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-lbj-50 utexas-lbj-50 https://lbj50.org >> migration-report.txt 2>&1
+./migrate-to-preview.sh utqs-lbj-wcs utexas-lbj-wcs https://lbjwcs.lbj.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-legal-affairs utexas-legal https://legal.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-mesoamerica utexas-mesoamerica https://utmesoamerica.org >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-mrsec utexas-mrsec https://mrsec.utexas.edu >> migration-report.txt 2>&1
+./migrate-to-preview.sh utqs-offcampus utexas-offcampus https://offcampus.utexas.edu >> migration-report.txt 2>&1
+./migrate-to-preview.sh utqs-office-365 utexas-office-365 https://office365.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-ombuds utexas-ombuds https://ombuds.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-parents-association utexas-parents-assoc https://parents.utexas.edu
+./migrate-to-preview.sh utqs-payroll utexas-payroll https://payroll.utexas.edu
+./migrate-to-preview.sh utqs-purchasing utexas-purchasing https://purchasing.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-staff-council utexas-staff-council https://staffcouncil.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-student-affairs utexas-student-affairs https://studentaffairs.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-texas-memorial-museum utexas-texas-memorial-museum https://tmm.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-texas-metro-observatory utexas-texas-metro-observatory https://tmo.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-texcep utexas-texcep https://texcep.education.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-title-ix utexas-title-ix https://titleix.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-trademarks utexas-trademarks https://trademarks.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-ttap utexas-ttap https://ttap.disabilitystudies.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-urban-lab utexas-urban-lab https://urbanlab.lbj.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-waggoner-center utexas-waggoner https://waggonercenter.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-webpublishing utexas-webpublishing https://webpublishing.utexas.edu >> migration-report.txt 2>&1 
+./migrate-to-preview.sh utqs-zoom utexas-zoom https://zoom.its.utexas.edu >> migration-report.txt 2>&1 
